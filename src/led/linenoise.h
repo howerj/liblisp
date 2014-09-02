efc134a0ad89ad903d44501d06022e4316722e45
Richard Howe <howe.r.j.89@gmail.com>
2014-09-02 19:33:44 +0100

    improved makefile, changed indentation

--- src/led/linenoise.h
+++ src/led/linenoise.h
@@ -38,29 +38,32 @@
 #define __LINENOISE_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-typedef struct linenoiseCompletions {
-  size_t len;
-  char **cvec;
-} linenoiseCompletions;
+  typedef struct linenoiseCompletions
+  {
+    size_t len;
+    char **cvec;
+  } linenoiseCompletions;
 
-typedef void(linenoiseCompletionCallback)(const char *, linenoiseCompletions *);
-void linenoiseSetCompletionCallback(linenoiseCompletionCallback *);
-void linenoiseAddCompletion(linenoiseCompletions *, const char *);
+  typedef void (linenoiseCompletionCallback) (const char *,
+					      linenoiseCompletions *);
+  void linenoiseSetCompletionCallback (linenoiseCompletionCallback *);
+  void linenoiseAddCompletion (linenoiseCompletions *, const char *);
 
-char *linenoise(const char *prompt);
-int linenoiseHistoryAdd(const char *line);
-int linenoiseHistorySetMaxLen(int len);
-int linenoiseHistorySave(const char *filename);
-int linenoiseHistoryLoad(const char *filename);
-void linenoiseClearScreen(void);
-void linenoiseSetMultiLine(int ml);
-void linenoisePrintKeyCodes(void);
+  char *linenoise (const char *prompt);
+  int linenoiseHistoryAdd (const char *line);
+  int linenoiseHistorySetMaxLen (int len);
+  int linenoiseHistorySave (const char *filename);
+  int linenoiseHistoryLoad (const char *filename);
+  void linenoiseClearScreen (void);
+  void linenoiseSetMultiLine (int ml);
+  void linenoisePrintKeyCodes (void);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __LINENOISE_H */
+#endif				/* __LINENOISE_H */
