0ce1273b52a39e998f7e60485900a40f9c3923a6
Richard Howe <howe.r.j.89@gmail.com>
2014-10-14 23:29:17 +0100

    Added example

--- src/prn/print.c
+++ src/prn/print.c
@@ -3,6 +3,36 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int main(void){
+/*taken from the man pages*/
+void foo(char *fmt, ...)
+{
+        va_list ap;
+        int d;
+        char c, *s;
+
+        va_start(ap, fmt);
+        while (*fmt)
+                switch (*fmt++) {
+                case 's':      /* string */
+                        s = va_arg(ap, char *);
+                        printf("string %s\n", s);
+                        break;
+                case 'd':      /* int */
+                        d = va_arg(ap, int);
+                        printf("int %d\n", d);
+                        break;
+                case 'c':      /* char */
+                        /* need a cast here since va_arg only
+                           takes fully promoted types */
+                        c = (char)va_arg(ap, int);
+                        printf("char %c\n", c);
+                        break;
+                }
+        va_end(ap);
+}
+
+int main(void)
+{
+        foo("sdc", "hello", 10, 'c');
         return 0;
 }
