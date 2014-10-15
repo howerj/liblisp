f0482715b8506195b2dfae2b7f623f7343e62c58
Richard Howe <howe.r.j.89@gmail.com>
2014-10-15 19:39:11 +0100

    reworking print function

--- src/prn/print.c
+++ src/prn/print.c
@@ -2,37 +2,47 @@
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "color.h"
 
-/*taken from the man pages*/
-void foo(char *fmt, ...)
+void printc(char *fmt, ...)
 {
         va_list ap;
         int d;
         char c, *s;
 
         va_start(ap, fmt);
-        while (*fmt)
-                switch (*fmt++) {
-                case 's':      /* string */
-                        s = va_arg(ap, char *);
-                        printf("string %s\n", s);
-                        break;
-                case 'd':      /* int */
-                        d = va_arg(ap, int);
-                        printf("int %d\n", d);
-                        break;
-                case 'c':      /* char */
-                        /* need a cast here since va_arg only
-                           takes fully promoted types */
-                        c = (char)va_arg(ap, int);
-                        printf("char %c\n", c);
-                        break;
+        while (*fmt){
+                char f;
+                if('%' == (f = *fmt++)){
+                        switch (*fmt++) {
+                        case '\0':
+                                return;
+                        case 's':      
+                                s = va_arg(ap, char *);
+                                fputs(s,stdout);
+                                break;
+                        case 'd':      /* int */
+                                d = va_arg(ap, int32_t);
+                                printf("%d", d);
+                                break;
+                        case 'c':     
+                                /* need a cast here since va_arg only
+                                   takes fully promoted types */
+                                c = (char)va_arg(ap, int);
+                                putchar(c);
+                                break;
+                        default:
+                                break;
+                        }
+                } else {
+                        putchar(f);
                 }
+        }
         va_end(ap);
 }
 
 int main(void)
 {
-        foo("sdc", "hello", 10, 'c');
+        printc("%s%d%c", "hello", 10, 'c');
         return 0;
 }
