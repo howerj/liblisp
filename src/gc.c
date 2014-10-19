666195ff62379a68155ccfa4e12457809b5b39f5
Richard Howe <howe.r.j.89@gmail.com>
2014-10-19 17:00:57 +0100

    Removing code guts

--- src/gc.c
+++ src/gc.c
@@ -65,41 +65,6 @@ expr gc_calloc(void)
  **/
 int gc_mark(expr root)
 {
-        if (NULL == root)
-                return false;
-
-        root->gc_mark = true;
-
-        switch (root->type) {
-        case S_LIST:
-                {
-                        size_t i;
-                        for (i = 0; i < root->len; i++)
-                                gc_mark(root->data.list[i]);
-                }
-                return false;
-        case S_PROC:
-                /** @todo Put the S_PROC structure into type.h **/
-                gc_mark(root->data.list[0]);
-                gc_mark(root->data.list[1]);
-                gc_mark(root->data.list[2]);
-                return false;
-        case S_PRIMITIVE:
-        case S_NIL:
-        case S_TEE:
-        case S_STRING:
-        case S_SYMBOL:
-        case S_INTEGER:
-        case S_ERROR:
-        case S_FILE:
-        case S_QUOTE:
-                break;
-        case S_LAST_TYPE:
-        default:
-                fputs("unmarkable type\n",stderr);
-                exit(EXIT_FAILURE);
-        }
-        return false;
 }
 
 /**
@@ -108,33 +73,6 @@ int gc_mark(expr root)
  **/
 void gc_sweep(void)
 {
-  /**@todo this really needs cleaning up**/
-        struct heap *ll, *pll;
-        if (NULL == heaplist.next)      /*pass first element, do not collect element */
-                return;
-        for (ll = heaplist.next, pll = &heaplist; ll != heaphead;) {
-                if (true == ll->x->gc_mark) {
-                        ll->x->gc_mark = false;
-                        pll = ll;
-                        ll = ll->next;
-                } else {
-                        gcinner(ll->x);
-                        ll->x = NULL;
-                        pll->next = ll->next;
-                        mem_free(ll);
-                        ll = pll->next;
-                }
-        }
-        if ((heaphead != &heaplist) && (NULL != heaphead->x)) {
-                if (true == heaphead->x->gc_mark) {
-                        ll->x->gc_mark = false;
-                } else {
-                        gcinner(heaphead->x);
-                        heaphead->x = NULL;
-                        mem_free(heaphead);
-                        heaphead = pll;
-                }
-        }
 }
 
 /*****************************************************************************/
@@ -146,49 +84,6 @@ void gc_sweep(void)
  **/
 static void gcinner(expr x)
 {
-        if (NULL == x)
-                return;
-
-        switch (x->type) {
-        case S_TEE:
-        case S_NIL:
-        case S_INTEGER:
-        case S_PRIMITIVE:
-                mem_free(x);
-                break;
-        case S_PROC:
-                mem_free(x->data.list);
-                mem_free(x);
-                break;
-        case S_LIST:
-                mem_free(x->data.list);
-                mem_free(x);
-                return;
-        case S_SYMBOL:
-                mem_free(x->data.symbol);
-                mem_free(x);
-                return;
-        case S_STRING:
-                mem_free(x->data.string);
-                mem_free(x);
-                return;
-        case S_ERROR:
-                /** @todo implement error support **/
-                mem_free(x);
-                return;
-        case S_FILE:
-               /** @todo implement file support **/
-                REPORT("UNIMPLEMENTED (TODO)");
-                break;
-        case S_QUOTE:
-                REPORT("UNIMPLEMENTED (TODO)");
-                break;
-        case S_LAST_TYPE:
-        default:               /* should never get here */
-                REPORT("free: not a known 'free-able' type");
-                exit(EXIT_FAILURE);
-                return;
-        }
 }
 
 /*****************************************************************************/
